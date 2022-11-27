805eeb581e7566c19b2fbecfdd2c7beb0a983398
Yuki Yamada <e155417k@st.u-gakugei.ac.jp>
2022-11-27 08:15:34 +0000

    add: pageコンポーネントを作成

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <AppAlert> This is an auto-imported component </AppAlert>
+  </div>
+</template>
+
+<style scoped></style>
