0e13780a41f530501621b3b28c34ed08fca0caa6
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2022-12-06 02:15:15 +0900

    feat: layoutコンポーネントの定義

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  margin: auto;
+  max-width: 1024px;
+}
+</style>
