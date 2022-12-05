e7c825fe67b777b8a40f1f4974225926da2dd90a
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2022-12-06 02:16:04 +0900

    feat: pagesコンポーネントを作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,42 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { CatResponse } from '~/types'
+
+const { pending, error, data, refresh } = useFetch<CatResponse[]>(
+  'https://api.thecatapi.com/v1/images/search?limit=10'
+)
+</script>
 
 <template>
   <div>
-    <h1>Welcome to the homepage</h1>
-    <AppAlert> This is an auto-imported component </AppAlert>
+    <div class="top">
+      <app-title />
+      <refresh-button @click="refresh()" />
+    </div>
+
+    <div>
+      {{ error?.message }}
+    </div>
+
+    <div v-show="pending" class="loading-message">
+      <p>データ取得中です…</p>
+    </div>
+
+    <div v-if="!pending && data" class="content">
+      <cat-card-list :cat-list="data" />
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.top {
+  text-align: center;
+}
+
+.content {
+  margin: auto;
+}
+
+.loading-message {
+  text-align: center;
+}
+</style>
